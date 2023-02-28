b7a6bb5968574337e50551f1164aaddef897c82d
ntvs <NickRibs@hotmail.com>
2023-02-28 16:29:37 -0500

    Added SocialLink component

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,5 @@
 <script>
+    import SocialLink from "./SocialLink.svelte";
 </script>
 
 <footer>
@@ -6,7 +7,8 @@
         <!--ntvs is a graphic designer and artist who has been active since 2012. Please use Twitter to contact.-->
         <a href="#top" class="top">Back to the top</a>
         <!-- <h3>&copy; 2012 till infinity</h3> -->
-        <a href="https://twitter.com/TiltStick">Twitter @TiltTtick</a>
+        <SocialLink options={{"icon": "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png", "link": "https://twitter.com/TiltStick", "text": "Twitter"}} />
+        <!-- <a href="https://twitter.com/TiltStick">Twitter @TiltTtick</a> -->
     </div>
 </footer>
 
