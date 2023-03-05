72c67a8548cafca6bd538009f02f4b27090555ad
ntvs <NickRibs@hotmail.com>
2023-03-04 23:01:36 -0500

    Edited footer component

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -4,11 +4,8 @@
 
 <footer>
     <div class="container">
-        <!--ntvs is a graphic designer and artist who has been active since 2012. Please use Twitter to contact.-->
         <a href="#top" class="top">Back to the top</a>
-        <!-- <h3>&copy; 2012 till infinity</h3> -->
-        <SocialLink options={{"icon": "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png", "link": "https://twitter.com/TiltStick"}} />
-        <!-- <a href="https://twitter.com/TiltStick">Twitter @TiltTtick</a> -->
+        <!-- <SocialLink options={{"icon": "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-twitter-social-media-round-icon-png-image_6315985.png", "link": "https://twitter.com/TiltStick"}} /> -->
     </div>
 </footer>
 
